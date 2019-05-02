54c541986fa5a505d78d1210f9c8f36ac3722402
Lidefi87 <lidefi87@hotmail.com>
2019-05-02 09:41:28 -0600

    Update TransferringFiles.R

--- TransferringFiles.R
+++ TransferringFiles.R
@@ -1,7 +1,7 @@
 ################################# Copying information across hard drives ##########################################
 # Author: Denisse Fierro Arcos
-# Version: 2 - allows code to be used when folders are contained within folders
-# Date last update: 2019-02-20
+# Version: 3 - Ignores folders in HDs like the recycle bin and spotlight searches
+# Date last update: 2019-05-02
 # What does it do?: This code lets you copy the entire contents of a hard drive/hard disk/server/USB stick to 
 # another hard drive/hard disk/server/USB stick. It can also be used to compare contents between hard drives/hard 
 # disks/servers/USB sticks
@@ -10,14 +10,17 @@
 #Clear workspace
 rm(list = ls())
 
+#Upload relevant libraries
+library(DescTools)
+
 ##################################################################################################################
 ############################ Update start and end directories below this line ####################################
 ##################################################################################################################
 #Write directory where all files you want to copy are currently found (use forward slash / only)
-startdir = "E:"
+startdir = "E:/"
 
 #Write the end directory - where all your files will be transfered to
-enddir = "Z:/ETP/Malpelo_2018"
+enddir = "Z:/Darwin and Wolf Stereo Sharks 2013-2018/DOVS 2014-2016"
 ##################################################################################################################
 ##################################################################################################################
 
@@ -25,11 +28,18 @@ enddir = "Z:/ETP/Malpelo_2018"
 ##################################################################################################################
 ################################## DO NOT CHANGE THE CODE BELOW THIS LINE ########################################
 ##################################################################################################################
+#List of folders to NOT be moved to the server
+NoMoveList = c("%RECYCLE.BIN%", "%fseventsd%", "%Spotlight%", "%Trashes%", "%System Volume Information%", 
+               "%TemporaryItems%")
+
 #Set working directory to "from disk"
 setwd(startdir)
 #Create a list of directories and files for "from disk"
 StartDir = list.dirs(recursive = T)
+#Delete files in recycle bin
+StartDir = StartDir[!StartDir %like any% NoMoveList]
 StartFiles = list.files(recursive = T)
+StartFiles = StartFiles[!StartFiles %like any% NoMoveList]
 
 #Set working directory to "to disk"
 setwd(enddir)
@@ -51,3 +61,4 @@ if(length(MoveFiles) > 0){
   for(i in seq_along(MoveFiles)){
     file.copy(paste(startdir, MoveFiles[i], sep = "/"), paste(enddir, MoveFiles[i], sep = "/"), recursive = F)}}
 ##################################################################################################################
+
